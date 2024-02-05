e17fa3287234108cee48b939b023a85bde5f7e81
MatteoOrlando <mat.orlando88@gmail.com>
2024-02-05 16:20:11 +0100

    w2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MyNav from './components/MyNav';
+import MyFooter from './components/MyFooter';
+import Welcome from './components/Welcome';
+// import AllTheBooks from './components/AllTheBooks'
+import { Container } from 'react-bootstrap';
+import BookList from './components/BookList';
+import fantasy from './components/data/fantasy.json';
+// import Table from './components/Table';
+
+// import { Col, Row } from 'react-bootstrap';
+// import { Component } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MyNav />
+      <Container>
+        <Welcome />
+        {/* <AllTheBooks /> */}
+        <BookList books={fantasy} />
+      </Container>
+      <MyFooter />
+    </>
   );
 }
+
+// class App extends Component() {
+//   render() {
+//     return (
+//       <>
+//         <header className="App-header">
+//           <Container>
+//             <Row>
+//               <Col></Col>
+//               <Col className="my-auto">
+//                 <BookList />
+//               </Col>
+//             </Row>
+//           </Container>
+//         </header>
+//       </>
+//     );
+//   }
+// }
 
 export default App;
